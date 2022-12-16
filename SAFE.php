1acb643b4e4eeec02a9e96779b8d5744997eefe5
Comrade-OP <80157534+Comrade-OP@users.noreply.github.com>
2022-12-16 21:01:32 +0530

    Create SAFE.php

---
+++ SAFE.php
@@ -0,0 +1,17 @@
+{
+"version" : 3,
+"Sd":"11020698",
+"Less" : true ,
+"Zero" : false ,
+"Shake" : false ,
+"MB" : false,
+"Aim" : false,
+"HS" : false,
+"Ipad" : true,
+"InstantHit" : true,
+"Cross" : true,
+"Xhit" : false,
+"Knock" : false,
+"SitScope" : false,
+"By" : false
+}
