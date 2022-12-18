20b9f3d4ca1cfa02626d6834731bb4c2a1b528f7
Comrade-OP <80157534+Comrade-OP@users.noreply.github.com>
2022-12-18 21:54:25 +0530

    Add files via upload

---
+++ Lizard.php
@@ -0,0 +1,17 @@
+{
+"version" : 3,
+"Sd":"10004954",
+"Less" : true ,
+"Zero" : false ,
+"Shake" : false ,
+"MB" : true,
+"Aim" : false,
+"HS" : true,
+"Ipad" : true,
+"InstantHit" : true,
+"Cross" : true,
+"Xhit" : true,
+"Knock" : false,
+"SitScope" : false,
+"By" : true
+}
